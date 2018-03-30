b9eadea561d96c36c9b1183cf861eb78a981ae3e
BarbarianTora <motorolllka@gmail.com>
2018-03-30 19:12:59 +0300

    +explosion + Fighter trails + CollisionScript

--- Assets/Scripts/CollisionScript.cs
+++ Assets/Scripts/CollisionScript.cs
@@ -3,10 +3,19 @@ using System.Collections;
 
 public class collisionScript : MonoBehaviour {
 
-	//for this to work both need colliders, one must have rigid body (spaceship) the other must have is trigger checked.
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+
+	}
+
 	void OnTriggerEnter (Collider col)
 	{
-		GameObject explosion = Instantiate(Resources.Load("SmallExplosionEffect", typeof(GameObject))) as GameObject;
+		GameObject explosion = Instantiate(Resources.Load( "WFXMR_ExplosiveSmoke", typeof( GameObject ) ) ) as GameObject;
 		explosion.transform.position = transform.position;
 		Destroy(col.gameObject);
 		Destroy (explosion, 2);
@@ -14,10 +23,10 @@ public class collisionScript : MonoBehaviour {
 
 		if (GameObject.FindGameObjectsWithTag("Player").Length == 0){
 
-			GameObject enemy = Instantiate(Resources.Load("Enemy", typeof(GameObject))) as GameObject;
-			GameObject enemy1 = Instantiate(Resources.Load("Enemy1", typeof(GameObject))) as GameObject;
-			GameObject enemy2 = Instantiate(Resources.Load("Enemy2", typeof(GameObject))) as GameObject;
-			GameObject enemy3 = Instantiate(Resources.Load("Enemy3", typeof(GameObject))) as GameObject;
+			GameObject enemy =  Instantiate( Resources.Load( "Fighter",  typeof( GameObject ) ) ) as GameObject;
+			GameObject enemy1 = Instantiate( Resources.Load( "Fighter1", typeof( GameObject ) ) ) as GameObject;
+			GameObject enemy2 = Instantiate( Resources.Load( "Fighter2", typeof( GameObject ) ) ) as GameObject;
+			GameObject enemy3 = Instantiate( Resources.Load( "Fighter3", typeof( GameObject ) ) ) as GameObject;
 
 		}
 
